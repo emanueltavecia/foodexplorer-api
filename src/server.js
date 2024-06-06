ce3f145abbbe330fadf3f5b3bc3dc212adddf3da
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-06-05 23:35:10 -0300

    add cors configs

--- src/server.js
+++ src/server.js
@@ -12,7 +12,12 @@ const routes = require('./routes')
 const cookieParser = require('cookie-parser')
 
 const app = express()
-app.use(cors())
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    credentials: true,
+  })
+)
 
 app.use(express.json())
 
