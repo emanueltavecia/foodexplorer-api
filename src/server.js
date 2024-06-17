2fd3efa5cd9f7530bfc0e12ba4fece716b9e4747
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-06-16 23:59:47 -0300

    add front-end deploy to cors origin

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use(
     origin: [
       'http://localhost:5173',
       'http://127.0.0.1:5173',
-      'https://foodexplorer-w.netlify.app/',
+      'https://foodexplorer-w.netlify.app',
     ],
     credentials: true,
   })
