5bd64e83d8be8fa22c44a7270dbf624d32911f7e
Emanuel Tavecia <emanueltavecia@hotmail.com>
2024-06-16 23:57:24 -0300

    add front-end deploy to cors origin

--- src/server.js
+++ src/server.js
@@ -14,7 +14,11 @@ const cookieParser = require('cookie-parser')
 const app = express()
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'https://foodexplorer-w.netlify.app/',
+    ],
     credentials: true,
   })
 )
